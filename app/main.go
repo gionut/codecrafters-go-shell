87229b2f649412048e3912ad37e3d34bfc403be5
root <root@ionut>
2026-02-11 23:05:19 +0200

    restructure code

--- app/main.go
+++ app/main.go
@@ -7,55 +7,81 @@ import (
 	"strings"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Print
+
+type Shell struct {
+	commands map[string]func([]string)
+	loop bool
+	reader *bufio.Scanner
+}
+
+func (s *Shell) _exit(args []string) {
+	s.loop = false
+}
+
+func (s Shell) _type(args []string) {
+	if len(args) == 0 {
+		return
+	}
+	
+	name := args[0]
+	if _, ok := s.commands[name]; ok {
+		fmt.Printf("%s is a shell builtin\n", name)
+	} else {
+		fmt.Printf("%s: not found\n", name)
+	}
+}
+
+func (s Shell) _echo(args []string) {
+	if len(args) == 0 {
+		return
+	}
+
+	fmt.Println(strings.Join(args, " "))
+}
+
+func (s *Shell) Loop() {
+	for s.loop {
+		fmt.Print("$ ")
+
+		if !s.reader.Scan() {
+            break
+        }
+
+		tokens := strings.Fields(s.reader.Text())
+        if len(tokens) == 0 {
+            continue
+        }
+		
+		command := tokens[0]
+		args := tokens[1:]
+		
+		if cmd, ok := s.commands[command]; ok {
+			cmd(args)
+		} else {
+			fmt.Println(command + ": command not found")
+		}
+	}
+}
+
+func NewShell() *Shell {
+	scanner := bufio.NewScanner(os.Stdin)
+    scanner.Buffer(make([]byte, 0, 1024), 1024)
+    
+	s := &Shell{
+        loop:     true,
+        commands: make(map[string]func([]string)),
+		reader: scanner,
+    }
+    
+    // Register commands here
+    s.commands["exit"] = s._exit
+    s.commands["type"] = s._type
+    s.commands["echo"] = s._echo
+    
+    return s
+}
 
 func main() {
-	commands := map[string]bool {
-		"exit": true,
-		"echo": true,
-		"type": true,
-	}
-
-	var loop = true
-	for loop {
-		fmt.Print("$ ")
-		
-		command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		command = strings.Trim(command, "\n")
-		
-		if command == "exit" {
-			loop = false
-			continue
-		}
-
-		var tokens = strings.SplitN(command, " ", 2)
-		if len(tokens) == 0 {
-			continue
-		}
-		command = tokens[0]
-		if command == "echo" {
-			if len(tokens) < 2 {
-				continue
-			}
-			var args = tokens[1]
-			fmt.Println(args)
-			continue
-		}
-		if command == "type" {
-			if len(tokens) < 2 {
-				continue
-			}
-			var args string = tokens[1]
-			_, ok := commands[args]
-			if ok {
-				fmt.Println(args + " is a shell builtin")
-			} else {
-				fmt.Println(args + ": not found")
-			}
-			continue
-		}
-
-		fmt.Println(command + ": command not found")
-	}
+	shell := NewShell()
+	shell.Loop()
 }
